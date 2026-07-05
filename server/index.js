ebef60b1663ba1f0f5e419330230347e7183bc38
Kashyap <mistrykashyap340@gmail.com>
2026-07-05 15:52:01 +0530

    Add test GET route to verify backend

--- server/index.js
+++ server/index.js
@@ -67,6 +67,9 @@ app.post('/contact', async (req, res) => {
   }
 });
 
+app.get('/',(req,res)=>res.send('Backend is running!'))
+
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
