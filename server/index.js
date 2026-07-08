3fd2deb299b3f1179a64a05251a249491e222976
Kashyap <mistrykashyap340@gmail.com>
2026-07-08 21:57:12 +0530

    Add GET /contact route for debugging

--- server/index.js
+++ server/index.js
@@ -29,6 +29,12 @@ const transporter = nodemailer.createTransport({
   },
 });
 
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
+
 app.post('/contact', async (req, res) => {
   const { firstName, lastName, email, phone, service, budget, message, source } = req.body;
 
